06d05c742b10303e5f5c862c798cb7493bc1e303
GuyDea <theguydea@gmail.com>
2026-03-13 22:57:39 +0100

    start button over the keyboard

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, input, output } from '@angular/core';
+import { GameStage } from '../../models/game.models';
 
 @Component({
   selector: 'app-keyboard',
@@ -8,12 +9,24 @@ import { Component, ChangeDetectionStrategy, input, output } from '@angular/core
   styleUrl: './keyboard.component.scss',
 })
 export class KeyboardComponent {
-  readonly keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'del', '0', '?'];
+  readonly keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'del', '0', '⨯'];
 
   disabled = input<boolean>(false);
+  showStart = input<boolean>(false);
+  stage = input<GameStage>('idle');
   keyPress = output<string>();
+  startGame = output<void>();
+  abort = output<void>();
 
   onKey(key: string): void {
     this.keyPress.emit(key);
+  }
+
+  onStartOrAbort(): void {
+    if (this.stage() === 'showing') {
+      this.abort.emit();
+    } else {
+      this.startGame.emit();
+    }
   }
 }
