3657426e8ef1d1363ba3bbb2385dc9295a0e905d
GuyDea <theguydea@gmail.com>
2026-04-04 22:20:25 +0200

    post platform migraiton cleanup

--- games/doitoo-mindflow/src/app/utils/delivery.util.ts
+++
@@ -1,9 +0,0 @@
-import { StationIdentity } from '../models/game.models';
-
-export function classifyDelivery(
-  shapeIdentity: StationIdentity,
-  stationIdentity: StationIdentity,
-): boolean {
-  return shapeIdentity.shapeType === stationIdentity.shapeType
-    && shapeIdentity.color === stationIdentity.color;
-}
